84978d6be9fa6a787381d100cedc1304b4671da0
Filip Zoricic <filipzoricic@Filips-MBP.fritz.box>
2019-07-17 23:21:49 +0200

    babel setup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
+import express from 'express'
 
 const app = express()
 
 app.get('/ping', (req, res)=>{
-    res.send('pong')
+    res.send('ping')
 })
 
 app.listen(80, ()=>{
